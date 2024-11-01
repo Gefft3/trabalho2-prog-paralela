738edd9bec91d8930004dbe4eb57547451f2d9a9
Arthur-Henrique-Andrade-Farias <arthur.h.a.farias@ufms.br>
2024-11-01 16:14:58 -0400

    feat: adicionar arquivos de teste e scripts de chamada para algoritmos

---
+++ algoritmo1.1/call.sh
@@ -0,0 +1,5 @@
+dataset="../datasets/ca_astroph.edgelist"
+k_cliques=4
+n_threads=20
+
+./programa $dataset $k_cliques $n_threads
---
+++ algoritmo1.2/call.sh
@@ -0,0 +1,5 @@
+dataset="../datasets/dblp.edgelist"
+k_cliques=5
+n_threads=20
+
+./programa $dataset $k_cliques $n_threads
