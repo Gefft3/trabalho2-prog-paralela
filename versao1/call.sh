f7ef8af8e905b9734db8692e05670a09a412779e
Geffte Caetano <gefft3@gmail.com>
2024-11-03 22:01:27 -0400

    Refactor dataset and clique size in call.sh script

--- versao1/call.sh
+++ versao1/call.sh
@@ -1,10 +1,10 @@
-dataset="../datasets/ca_astroph.edgelist"
+dataset="../datasets/dblp.edgelist"
 
-k_cliques=5
+k_cliques=6
 
-n_threads=20
+n_threads=16
 
-metodo="guided" #pode ser "static" "dynamic" ou "guided"
+metodo="dynamic" #pode ser "static" "dynamic" ou "guided"
 chunk=10 #mudar apenas se for usar o dynamic, senao nao faz diferenca
 
 ./programa $dataset $k_cliques $n_threads $metodo $chunk
--- versao2/call.sh
+++ versao2/call.sh
@@ -4,3 +4,9 @@ n_threads=16
 carga_roubada=100
 
 ./programa $dataset $k_cliques $n_threads $carga_roubada
+
+k_cliques=6
+dataset="../datasets/dblp.edgelist"
+./programa $dataset $k_cliques $n_threads $carga_roubada
+
+bash ../versao1/call.sh
